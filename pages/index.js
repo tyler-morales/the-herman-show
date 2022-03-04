d86fa08164fe46c7fb7b4b084b6b3b287bce8ffe
tyler-morales <tyler.morales.dev@gmail.com>
2022-03-04 13:20:00 -0600

    ✨ Finish hero component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Hero from '../components/home/Hero'
 import Nav from '../components/Nav'
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   )
 }
