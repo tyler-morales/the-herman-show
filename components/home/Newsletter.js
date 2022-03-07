5e4077d5728c36eebfc4f9147438bb72ea80ae81
tyler-morales <tyler.morales.dev@gmail.com>
2022-03-07 13:10:32 -0600

    ✨  Add contest rules to about page

--- components/home/Newsletter.js
+++ components/home/Newsletter.js
@@ -6,7 +6,7 @@ export default function Newsletter() {
     <section className="bg-[url('../public/images/radio-bg.jpeg')] bg-cover h-[600px]">
       <div className="px-5 md:px-0 flex justify-center items-center h-[600px]">
         <div className="flex flex-col gap-6 text-center text-white">
-          <h2 className="text-3xl font-bold mb-4">
+          <h2 className="mb-4 text-3xl font-bold">
             Sign up for our Monthly Newsletter
           </h2>
           <p className="max-w-lg">
@@ -17,7 +17,7 @@ export default function Newsletter() {
             href="https://blogspot.us8.list-manage.com/subscribe?u=edd69c1aa277bd5a1709fd621&id=c2c3c37fbd"
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-blue-700 text-white w-max m-auto px-6 py-4 text-lg rounded-lg mt-6 block hover:scale-95 transition-all">
+            className="block px-6 py-4 m-auto mt-6 text-lg text-white transition-all bg-blue-700 rounded-lg w-max hover:scale-95">
             Join the mailing list
           </a>
         </div>
--- pages/about.js
+++ pages/about.js
@@ -9,6 +9,16 @@ export default function about() {
       <Hero />
       <TheHost />
       <TheTeam />
+      <div>
+        <h2 className="text-3xl font-bold text-center">Contest Rules</h2>
+        <a
+          className="block px-6 py-4 m-auto mt-6 text-lg text-white transition-all bg-blue-700 rounded-lg w-max hover:scale-95"
+          href="/contest_rules.pdf"
+          target="_blank"
+          rel="noopener noreferrer">
+          View Contest Rules
+        </a>
+      </div>
     </main>
   )
 }
