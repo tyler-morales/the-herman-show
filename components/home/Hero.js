c7c929287cf71a5b7af9a576928b7b9f7623e363
tyler-morales <tyler.morales.dev@gmail.com>
2022-03-07 19:16:15 -0600

    💄  Upate button hover state Scale 95% CTA button

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -8,23 +8,23 @@ export default function Hero() {
   return (
     <div className="mt-6 md:mt-10">
       <h1 className="text-center">
-        <span className="text-3xl block">The</span>
-        <span className="text-5xl md:text-7xl block font-bold">
+        <span className="block text-3xl">The</span>
+        <span className="block text-5xl font-bold md:text-7xl">
           Herman Show
         </span>
       </h1>
-      <p className="text-center text-lg mt-4">A Talk & Music Radio Show</p>
-      <figure className="mt-6 m-auto max-w-7xl relative">
+      <p className="mt-4 text-lg text-center">A Talk & Music Radio Show</p>
+      <figure className="relative m-auto mt-6 max-w-7xl">
         <Image
           src="/../public/images/hero.jpeg"
           height={screenWidth > 640 ? '400px' : '700'}
           width={1000}
           layout="responsive"
           alt="Brandon Herman recording a Radio show in the WLTL Studio"
-          className=" object-cover w-full h-full"
+          className="object-cover w-full h-full "
         />
         <Link href="/episodes">
-          <a className="absolute bottom-6 z-10 text-white bg-blue-700 rounded-lg py-4 px-8 m-auto left-0 right-0 w-max hover:bg-blue-800 transition-all">
+          <a className="absolute left-0 right-0 z-10 px-8 py-4 m-auto text-white transition-all bg-blue-700 rounded-lg bottom-6 w-max hover:scale-95">
             Listen Now
           </a>
         </Link>
